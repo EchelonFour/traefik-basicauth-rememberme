51ef6f4dc71fa02128ea795278e040c60d9e5bbc
Frank Fenton <echelon.iiii@gmail.com>
2021-09-14 22:46:10 +1000

    ✨ Let the server send back basic auth challenges

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 #[macro_use] extern crate rocket;
+use rocket::http::Header;
+use rocket::response::content::Html;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+struct WWWAuthenticate {
+    realm: String
+}
+
+impl From<WWWAuthenticate> for Header<'static> {
+    fn from(www_authenticate: WWWAuthenticate) -> Self {
+        Header::new("WWW-Authenticate", format!("Basic realm=\"{}\"", www_authenticate.realm))
+    }
+}
+
+#[derive(Responder)]
+#[response(status = 401)]
+struct AuthChallengeResponse {
+    inner: Html<&'static str>,
+    header: WWWAuthenticate
+}
+#[get("/<_..>")]
+fn authenticate() -> AuthChallengeResponse {
+    AuthChallengeResponse {
+        inner: Html("<wow></wow>"),
+        header: WWWAuthenticate { realm: "Hey, are you cool?".to_string()}
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    let data = "user:$apr1$lZL6V/ci$eIMz/iKDkbtys/uU7LEK00";
+    let htpasswd = htpasswd_verify::Htpasswd::new(data);
+    rocket::build()
+        .attach(rocket::shield::Shield::new())
+        .manage(htpasswd)
+        .mount("/", routes![authenticate])
 }
