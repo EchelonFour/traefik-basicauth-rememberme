ee1c2a93d7261769feb915193d3741d6f74769e3
Frank Fenton <echelon.iiii@gmail.com>
2021-09-15 23:03:27 +1000

    🔧 Move all the options to the rocket config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 #[macro_use] extern crate rocket;
+use rocket::figment::providers::Serialized;
 use rocket::http::{Cookie, CookieJar};
+use rocket::State;
+use rocket::fairing::AdHoc;
+use rocket::serde::Deserialize;
+use rocket::serde::Serialize;
 use htpasswd_verify::Htpasswd;
 
 mod user;
@@ -8,30 +13,53 @@ use user::User;
 mod auth_challenge;
 use auth_challenge::AuthChallengeResponse;
 
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct AppConfig {
+    realm: String,
+    cookie_name: String,
+    htpasswd_path: String,
+    user_header: String,
+}
+impl Default for AppConfig {
+    fn default() -> Self {
+        AppConfig {
+            realm: "Please sign in".to_string(),
+            cookie_name: "_auth_remember_me".to_string(),
+            htpasswd_path: ".htpasswd".to_string(),
+            user_header: "x-user".to_string(),
+        }
+    }
+}
+
+
 #[get("/<_..>", rank = 1)]
-fn verify_user(user: User, cookies: &CookieJar<'_>) -> User {
+fn verify_user(user: User, cookies: &CookieJar<'_>, app_config: &State<AppConfig>) -> User {
     if !user.from_cookie {
-        cookies.add_private(Cookie::build("remembered_user", user.id.clone())
+        cookies.add_private(Cookie::build(app_config.cookie_name.to_owned(), user.id.to_owned())
             .permanent()
             .finish());
     }
     user
 }
 
-
-
 #[get("/<_..>", rank = 2)]
-fn authenticate(cookies: &CookieJar<'_>) -> AuthChallengeResponse {
-    cookies.remove(Cookie::named("remembered_user"));
-    AuthChallengeResponse::new("Hey, are you cool?".into()) 
+fn authenticate(cookies: &CookieJar<'_>, app_config: &State<AppConfig>) -> AuthChallengeResponse {
+    cookies.remove(Cookie::named(app_config.cookie_name.to_owned()));
+    AuthChallengeResponse::new(app_config.realm.to_owned()) 
 }
 
 #[launch]
 fn rocket() -> _ {
     let data = "user:$apr1$lZL6V/ci$eIMz/iKDkbtys/uU7LEK00";
     let htpasswd = Htpasswd::new(data);
-    rocket::build()
+    let rocket = rocket::build();
+    let figment = rocket.figment().clone()
+        .join(Serialized::defaults(AppConfig::default()));
+    rocket
+        .configure(figment)
         .attach(rocket::shield::Shield::new())
+        .attach(AdHoc::config::<AppConfig>())
         .manage(htpasswd)
         .mount("/", routes![verify_user, authenticate])
 }
--- src/user.rs
+++ src/user.rs
@@ -6,23 +6,26 @@ use htpasswd_verify::Htpasswd;
 use http::header::AUTHORIZATION;
 use http_auth_basic::Credentials;
 
+use crate::AppConfig;
+
 pub struct User {
   pub id: String,
   pub from_cookie: bool,
+  header_name: String,
 }
 
-fn get_cookie_user(request: &'_ Request<'_>) -> Option<User> {
+fn get_cookie_user(request: &'_ Request<'_>, app_config: &State<AppConfig>) -> Option<User> {
   request.cookies()
-      .get_private("remembered_user")
+      .get_private(&app_config.cookie_name)
       .and_then(|cookie| cookie.value().parse().ok())
-      .map(|user| User { id: user, from_cookie: true })
+      .map(|user| User { id: user, from_cookie: true, header_name: app_config.user_header.to_owned() })
 }
-fn get_auth_user(request: &'_ Request<'_>, htpasswd: &State<Htpasswd>) -> Option<User> {
+fn get_auth_user(request: &'_ Request<'_>, htpasswd: &State<Htpasswd>, app_config: &State<AppConfig>) -> Option<User> {
   request.headers()
       .get_one(AUTHORIZATION.as_str())
       .and_then(|auth| Credentials::from_header(auth.to_string()).ok())
       .and_then(|credentials| if htpasswd.check(&credentials.user_id, &credentials.password) { Some(credentials.user_id) } else { None })
-      .map(|valid_user| User { id: valid_user, from_cookie: false })
+      .map(|valid_user| User { id: valid_user, from_cookie: false, header_name: app_config.user_header.to_owned() })
 } 
 
 #[rocket::async_trait]
@@ -30,8 +33,9 @@ impl<'r> FromRequest<'r> for User {
   type Error = ();
 
   async fn from_request(request: &'r Request<'_>) -> Outcome<Self, Self::Error> {
+      let app_config: &State<AppConfig> = try_outcome!(request.guard::<&State<AppConfig>>().await);
       let htpasswd: &State<Htpasswd> = try_outcome!(request.guard::<&State<Htpasswd>>().await);
-      get_cookie_user(request).or_else(|| get_auth_user(request, htpasswd)).or_forward(())
+      get_cookie_user(request, app_config).or_else(|| get_auth_user(request, htpasswd, app_config)).or_forward(())
   }
 }
 
@@ -39,7 +43,7 @@ impl<'r> FromRequest<'r> for User {
 impl<'r> Responder<'r, 'static> for User {
   fn respond_to(self, _: &'r Request<'_>) -> response::Result<'static> {
       Response::build()
-          .raw_header("x-user", self.id)
+          .raw_header(self.header_name, self.id)
           .ok()
   }
 }
