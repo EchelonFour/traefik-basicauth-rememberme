9761bc320c30437c225c8d6f10623a1756694b82
Frank Fenton <echelon.iiii@gmail.com>
2021-09-18 01:49:41 +1000

    🎨 Fix formatting

--- src/auth_challenge.rs
+++ src/auth_challenge.rs
@@ -8,22 +8,30 @@ const AUTH_RESPONSE_BODY: &str = r#"<html>
 </body>
 </html>"#;
 struct WWWAuthenticate {
-  realm: String
+    realm: String,
 }
 
 impl From<WWWAuthenticate> for Header<'static> {
-  fn from(www_authenticate: WWWAuthenticate) -> Self {
-      Header::new(WWW_AUTHENTICATE.as_str(), format!("Basic realm=\"{}\"", www_authenticate.realm))
-  }
+    fn from(www_authenticate: WWWAuthenticate) -> Self {
+        Header::new(
+            WWW_AUTHENTICATE.as_str(),
+            format!("Basic realm=\"{}\"", www_authenticate.realm),
+        )
+    }
 }
 
 #[derive(Responder)]
 #[response(status = 401)]
 pub struct AuthChallengeResponse {
     inner: Html<&'static str>,
-    header: WWWAuthenticate
+    header: WWWAuthenticate,
 }
 
 impl AuthChallengeResponse {
-    pub fn new(realm: String) -> Self { Self { inner: Html(AUTH_RESPONSE_BODY), header: WWWAuthenticate { realm } } }
+    pub fn new(realm: String) -> Self {
+        Self {
+            inner: Html(AUTH_RESPONSE_BODY),
+            header: WWWAuthenticate { realm },
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,43 @@
-use std::{convert::Infallible, sync::Arc};
-use std::error::Error;
+use cookie::{Cookie, CookieJar};
+use htpasswd_verify::Htpasswd;
+use http::header::{AUTHORIZATION, COOKIE};
 use http_auth_basic::Credentials;
+use std::error::Error;
+use std::{convert::Infallible, sync::Arc};
 use tracing::info;
 use warp::{Filter, Rejection};
-use cookie::{Cookie, CookieJar};
-use http::header::{AUTHORIZATION, COOKIE};
-use htpasswd_verify::Htpasswd;
 
 mod config;
 use crate::config::AppConfig;
 
 fn cookie_jar() -> impl Filter<Extract = (CookieJar,), Error = Rejection> + Copy {
-  warp::header::optional(COOKIE.as_str()).and_then(|cookie_header: Option<String>| {
-    let mut jar = CookieJar::new();
-    if let Some(cookies_raw) = cookie_header {
-      let cookies = cookies_raw.split(';')
-        .filter_map(|cookie| {
-          let cookie = cookie.to_owned();
-          let mut cookie = cookie.splitn(2, "=");
-          let key = cookie.next()?.trim();
-          let val = cookie.next()?.trim();
-          Some((key.to_owned(), val.to_owned()))
-        });
-      for (cookie_key, cookie_value) in cookies {
-        jar.add_original(Cookie::new(cookie_key, cookie_value));
-      }
-    }
-    std::future::ready(Ok::<_, Infallible>(jar))
-  })
+    warp::header::optional(COOKIE.as_str()).and_then(|cookie_header: Option<String>| {
+        let mut jar = CookieJar::new();
+        if let Some(cookies_raw) = cookie_header {
+            let cookies = cookies_raw.split(';').filter_map(|cookie| {
+                let cookie = cookie.to_owned();
+                let mut cookie = cookie.splitn(2, "=");
+                let key = cookie.next()?.trim();
+                let val = cookie.next()?.trim();
+                Some((key.to_owned(), val.to_owned()))
+            });
+            for (cookie_key, cookie_value) in cookies {
+                jar.add_original(Cookie::new(cookie_key, cookie_value));
+            }
+        }
+        std::future::ready(Ok::<_, Infallible>(jar))
+    })
 }
 
 fn auth_header_exists() -> impl Filter<Extract = (Credentials,), Error = Rejection> + Copy {
-  warp::header::header(AUTHORIZATION.as_str())
-    .and_then(|auth_header: String| {
-      std::future::ready(Credentials::from_header(auth_header)
-        .map_err(|auth_error| {
-          info!("Invalid authorization header, ignoring. {}", error = auth_error);
-          warp::reject::not_found()
-        })
-      )
+    warp::header::header(AUTHORIZATION.as_str()).and_then(|auth_header: String| {
+        std::future::ready(Credentials::from_header(auth_header).map_err(|auth_error| {
+            info!(
+                "Invalid authorization header, ignoring. {}",
+                error = auth_error
+            );
+            warp::reject::not_found()
+        }))
     })
 }
 #[derive(Debug)]
@@ -46,41 +45,53 @@ struct InvalidUser;
 
 impl warp::reject::Reject for InvalidUser {}
 
-fn auth_header_valid(htpasswd: Arc<Htpasswd>) -> impl Filter<Extract = (String,), Error = Rejection> + Clone {
-  auth_header_exists().and(warp::any().map(move || htpasswd.clone()))
-    .and_then(|credentials: Credentials, pass: Arc<Htpasswd>| {
-      std::future::ready(if pass.check(&credentials.user_id, &credentials.password) {
-        Ok(credentials.user_id)
-      } else {
-        Err(warp::reject::custom(InvalidUser))
-      })
-    })
+fn auth_header_valid(
+    htpasswd: Arc<Htpasswd>,
+) -> impl Filter<Extract = (String,), Error = Rejection> + Clone {
+    auth_header_exists()
+        .and(warp::any().map(move || htpasswd.clone()))
+        .and_then(|credentials: Credentials, pass: Arc<Htpasswd>| {
+            std::future::ready(if pass.check(&credentials.user_id, &credentials.password) {
+                Ok(credentials.user_id)
+            } else {
+                Err(warp::reject::custom(InvalidUser))
+            })
+        })
 }
 
 fn setup_logging() {
-  tracing_subscriber::fmt::fmt()
-  .with_env_filter(tracing_subscriber::EnvFilter::try_from_default_env()
-    .or_else(|error| {
-      if Some(&std::env::VarError::NotPresent) != error.source().unwrap().downcast_ref::<std::env::VarError>() {
-        println!("Could not parse log options, defaulting ({})", error);
-      }
-      tracing_subscriber::EnvFilter::try_new("info")
-    })
-    .expect("Could not set up log filter correctly"))
-  .init();
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .or_else(|error| {
+                    if Some(&std::env::VarError::NotPresent)
+                        != error.source().unwrap().downcast_ref::<std::env::VarError>()
+                    {
+                        println!("Could not parse log options, defaulting ({})", error);
+                    }
+                    tracing_subscriber::EnvFilter::try_new("info")
+                })
+                .expect("Could not set up log filter correctly"),
+        )
+        .init();
 }
 
 #[tokio::main]
 async fn main() {
     setup_logging();
     let config = Arc::new(AppConfig::new().expect("Could not load config"));
-    let htpasswd_contents = std::fs::read_to_string(config.htpasswd_path.clone()).expect("Could not read htpasswd file");
+    let htpasswd_contents = std::fs::read_to_string(config.htpasswd_path.clone())
+        .expect("Could not read htpasswd file");
     let htpasswd = Arc::new(Htpasswd::new(htpasswd_contents));
-    let auth_route = auth_header_valid(htpasswd).and(cookie_jar())
-    .map(|auth, cookie: CookieJar| {
-      let actual_cookie = cookie.get("_auth_remember_me");
-      format!("{:#?} : {:#?}", auth, actual_cookie)
-    });
+    let auth_route =
+        auth_header_valid(htpasswd)
+            .and(cookie_jar())
+            .map(|auth, cookie: CookieJar| {
+                let actual_cookie = cookie.get("_auth_remember_me");
+                format!("{:#?} : {:#?}", auth, actual_cookie)
+            });
 
-    warp::serve(auth_route.with(warp::trace::request())).run(config.listen).await;
+    warp::serve(auth_route.with(warp::trace::request()))
+        .run(config.listen)
+        .await;
 }
