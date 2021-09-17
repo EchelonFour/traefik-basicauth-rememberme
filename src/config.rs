e34ac14366cf6e643b7bb85166011553ebbfd6f4
Frank Fenton <echelon.iiii@gmail.com>
2021-09-18 04:48:33 +1000

    ✨ Add reading a secret from the config

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,17 @@
 use std::{env, net::SocketAddr};
 use config::{ConfigError, Config, File, Environment};
+use cookie::Key;
 use serde::Deserialize;
+use derivative::Derivative;
+use tracing::warn;
+
+#[derive(Derivative)]
+#[derivative(Debug)]
+pub struct Secret {
+    #[derivative(Debug="ignore")]
+    pub inner: Key,
+    default: bool,
+}
 
 #[derive(Debug, Deserialize)]
 #[serde(default)]
@@ -10,7 +21,9 @@ pub struct AppConfig {
     pub htpasswd_path: String,
     pub user_header: String,
     pub listen: SocketAddr,
+    pub secret: Secret,
 }
+
 impl Default for AppConfig {
     fn default() -> Self {
         AppConfig {
@@ -19,6 +32,7 @@ impl Default for AppConfig {
             htpasswd_path: ".htpasswd".to_string(),
             user_header: "x-user".to_string(),
             listen: ([0, 0, 0, 0], 8000).into(),
+            secret: Secret { inner: cookie::Key::generate(), default: true },
         }
     }
 }
@@ -36,6 +50,26 @@ impl AppConfig {
 
       c.merge(Environment::with_prefix("app"))?;
 
-      c.try_into()
+      let final_config = c.try_into();
+      if Some(true) == final_config.as_ref().map(|x: &AppConfig| x.secret.default).ok() {
+          warn!("Using randomly generated secret. Be sure to set a 32 byte, base64 string in the settings.")
+      }
+      final_config
   }
+}
+
+impl<'de> Deserialize<'de> for Secret {
+    fn deserialize<D: serde::de::Deserializer<'de>>(de: D) -> Result<Self, D::Error> {
+        use base64::decode;
+        let secret_base64 = String::deserialize(de)?;
+        let secret_bytes = decode(secret_base64)
+            .map_err(|error| serde::de::Error::custom(format!("failed to parse base64 {}", error)))?;
+        if secret_bytes.len() < 32 {
+            return Err(serde::de::Error::invalid_length(secret_bytes.len(), &"more than 32 bytes"));
+        }
+        Ok(Secret {
+            inner: Key::derive_from(&secret_bytes),
+            default: false,
+        })
+    }
 }
