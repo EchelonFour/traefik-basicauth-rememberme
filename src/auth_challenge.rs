6d5cb38c1733178db0ab6d2929e01d9d10bf1c15
Frank Fenton <echelon.iiii@gmail.com>
2021-09-19 03:48:45 +1000

    🐛 Make unauth endpoint respond with correct status

--- src/auth_challenge.rs
+++ src/auth_challenge.rs
@@ -1,6 +1,9 @@
-use http::header::{WWW_AUTHENTICATE, SET_COOKIE};
-use warp::http::{Response, Result};
 use crate::CONFIG;
+use http::{
+    header::{SET_COOKIE, WWW_AUTHENTICATE},
+    StatusCode,
+};
+use warp::http::{Response, Result};
 
 const AUTH_RESPONSE_BODY: &str = r#"<html>
 <head><title>401 Authorization Required</title></head>
@@ -10,7 +13,10 @@ const AUTH_RESPONSE_BODY: &str = r#"<html>
 </html>"#;
 
 pub fn make_challenge_response(cookies: Option<cookie::Delta>) -> Result<Response<&'static str>> {
-    let mut response = Response::builder().header(WWW_AUTHENTICATE, format!("Basic realm=\"{}\"", &CONFIG.realm));
+    let mut response = Response::builder().status(StatusCode::UNAUTHORIZED).header(
+        WWW_AUTHENTICATE,
+        format!("Basic realm=\"{}\"", &CONFIG.realm),
+    );
     if let Some(cookies_real) = cookies {
         for set_cookie in cookies_real {
             response = response.header(SET_COOKIE, set_cookie.to_string());
