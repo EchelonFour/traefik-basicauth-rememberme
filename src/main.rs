e7d363e7f1bad9ed2876c8cc25c24a56050f9404
Frank Fenton <echelon.iiii@gmail.com>
2021-09-22 06:28:47 +1000

    🐛 Try redirecting to ourself to set the cookie

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use crate::cookie::{cookie_jar, has_decrypted_cookie, make_auth_cookie, Cookie, 
 use authentication::auth_header_exists;
 use std::convert::{Infallible, TryInto};
 use std::error::Error;
+use tracing::error;
 use user::User;
 use warp::{Filter, Rejection};
 
@@ -54,15 +55,14 @@ async fn main() {
             std::future::ready(cookie.try_into().map_err(|_| warp::reject::not_found()))
         })
         .and_then(validate_credentials)
-        .map(|user: User| response::make_valid_response(&user.user_id, None));
-    let auth_route = auth_header_exists()
-        .and_then(validate_credentials)
+        .map(|user: User| response::make_valid_response(&user.user_id));
+    let auth_route = warp::header::<String>("X-Forwarded-Uri")
+        .and(auth_header_exists().and_then(validate_credentials))
         .and(cookie_jar())
-        .map(|user: User, mut jar: CookieJar| {
+        .map(|original_url: String, user: User, mut jar: CookieJar| {
             let mut private_jar = jar.private_mut();
-            let user_id = user.user_id.to_owned();
             private_jar.add(user.into());
-            response::make_valid_response(&user_id, Some(jar.delta()))
+            response::make_cookie_response(&original_url, jar.delta())
         });
     let unauthorised = cookie_jar().map(|mut jar: CookieJar| {
         if None != jar.get(&CONFIG.cookie_name) {
@@ -76,6 +76,7 @@ async fn main() {
             .or(auth_route)
             .or(unauthorised)
             .recover(|error| async move {
+                error!("Issue validating user. {:?}", error);
                 Ok::<_, Infallible>(warp::reply::with_header(
                     response::make_challenge_response(None),
                     "x-error-message",
--- src/response.rs
+++ src/response.rs
@@ -1,7 +1,7 @@
-use crate::CONFIG;
 use crate::cookie::Delta;
+use crate::CONFIG;
 use http::{
-    header::{SET_COOKIE, WWW_AUTHENTICATE},
+    header::{LOCATION, SET_COOKIE, WWW_AUTHENTICATE},
     response::Builder,
     StatusCode,
 };
@@ -14,11 +14,16 @@ const AUTH_RESPONSE_BODY: &str = r#"<html>
 </body>
 </html>"#;
 
-pub fn make_valid_response(
-    user_id: &str,
-    cookies: Option<Delta>,
-) -> Result<Response<&'static str>> {
-    let mut response = Response::builder().header(CONFIG.user_header.as_str(), user_id);
+pub fn make_valid_response(user_id: &str) -> Result<Response<&'static str>> {
+    Response::builder()
+        .header(CONFIG.user_header.as_str(), user_id)
+        .body("")
+}
+
+pub fn make_cookie_response(redirect_url: &str, cookies: Delta) -> Result<Response<&'static str>> {
+    let mut response = Response::builder()
+        .status(StatusCode::TEMPORARY_REDIRECT)
+        .header(LOCATION, redirect_url);
     response = add_cookies(response, cookies);
     response.body("")
 }
@@ -28,15 +33,15 @@ pub fn make_challenge_response(cookies: Option<Delta>) -> Result<Response<&'stat
         WWW_AUTHENTICATE,
         format!("Basic realm=\"{}\"", &CONFIG.realm),
     );
-    response = add_cookies(response, cookies);
+    if let Some(cookies) = cookies {
+        response = add_cookies(response, cookies);
+    }
     response.body(AUTH_RESPONSE_BODY)
 }
 
-fn add_cookies(mut response: Builder, cookies: Option<Delta>) -> Builder {
-    if let Some(cookies_real) = cookies {
-        for set_cookie in cookies_real {
-            response = response.header(SET_COOKIE, set_cookie.to_string());
-        }
+fn add_cookies(mut response: Builder, cookies: Delta) -> Builder {
+    for set_cookie in cookies {
+        response = response.header(SET_COOKIE, set_cookie.to_string());
     }
     response
 }
